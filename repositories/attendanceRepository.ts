8bf6666e0272b6ed74d165b85cdead1dda37576c
git-dariel <104293978+git-dariel@users.noreply.github.com>
2025-06-25 21:13:41 +0800

    feat: update today

--- repositories/attendanceRepository.ts
+++ repositories/attendanceRepository.ts
@@ -177,7 +177,7 @@ export class AttendanceRepository {
     try {
       return await Attendance.find(filter)
         .sort({ createdAt: -1 })
-        .populate("studentId", "firstName lastName email section strand image, studentId, gradeLevel, remarks")
+        .populate("studentId", "firstName lastName email section strand image studentId gradeLevel remarks")
         .populate("subjectId", "code name schedule")
     } catch (error) {
       throw new AppError("Failed to fetch attendance records", 500);
